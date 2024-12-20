9f4c21128c42fbf049b04f3a66a8425d47c1b69b
Pete Looney <petelooney81@gmail.com>
2024-12-20 10:31:26 -0600

    :sparkles: Add support for parsing and converting SQL INSERT statements
    This commit introduces functionality to handle SQL INSERT statements, mapping their structure from jsqlparser to HoneySQL. Key changes include new methods for converting tables, columns, and values in INSERT statements, along with updated test cases and modularization of helper functions. This improves parsing flexibility and alignment with the existing SELECT statement handling.

--- src/plooney81/nectar/sql.clj
+++ src/plooney81/nectar/sql.clj
@@ -4,10 +4,12 @@
             [plooney81.nectar.jsql :as jsql]
             [plooney81.nectar.sql.expression]
             [plooney81.nectar.sql.impl :as impl]
+            [plooney81.nectar.sql.insert]
             [plooney81.nectar.sql.select]
             [plooney81.nectar.sql.select-item]
             [plooney81.nectar.sql.set-operation])
-  (:import (net.sf.jsqlparser.statement.select Select)))
+  (:import (net.sf.jsqlparser.statement.insert Insert)
+           (net.sf.jsqlparser.statement.select Select)))
 
 (defmethod impl/jsql->honey-adapter :default [_honey jsql]
   (throw (IllegalArgumentException.
@@ -15,6 +17,9 @@
 
 (defmethod impl/jsql->honey-adapter Select [honey jsql]
   (impl/select->honey honey jsql))
+
+(defmethod impl/jsql->honey-adapter Insert [honey jsql]
+  (impl/insert->honey honey jsql))
 
 (defn ripen
   "Process of turning nectar into honey. Accepts a raw-sql string and returns a honeysql map."
@@ -28,11 +33,10 @@
       (-> (ripen sql-string)
           (honey/format {:inline true :pretty true}))))
 
-  (-> (str "SELECT json_column -> 'name' -> 'another'")
-      #_(ripen)
-      (around-the-horn))
+  (around-the-horn "SELECT json_column -> 'name' -> 'another'")
 
-  (honey/format {:select [[[:? :json_column "name"]]]} {:inline true :pretty true})
-  (honey/format {:select [[:- :json_column "age"]]} {:inline true :pretty true})
+  (around-the-horn "INSERT INTO users AS u (id, username, email) VALUES (1, 'john_doe', 'john@example.com')")
+
+  (around-the-horn "INSERT INTO users AS u (id, username, email) VALUES (1, 'john_doe', 'john@example.com'), (2, 'admin', 'admin@example.com')")
 
   )
